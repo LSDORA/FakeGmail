60a529a37bc6948b6a59cebb3f724c014486087c
Aymerick <120599499+LSDORA@users.noreply.github.com>
2024-01-07 23:23:14 +0100

    3

--- js/app.js
+++ js/app.js
@@ -7,29 +7,3 @@ function changeSection(sectionName) {
     document.querySelector('.' + sectionName).style.opacity = 1;
 }
 
-function submitForm(event) {
-    event.preventDefault();
-
-    // Récupérer les données du formulaire
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
-    var prénom = document.getElementById('prénom').value;
-    var nom = document.getElementById('nom').value;
-
-    // Appeler la fonction PHP pour envoyer l'e-mail
-    sendEmail(prénom,nom,email, password);
-}
-
-function sendEmail(prénom,nom, email, password) {
-    // Utiliser une requête AJAX pour appeler le script PHP
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "send_email.php", true);
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            // Gérer la réponse si nécessaire
-            console.log(xhr.responseText);
-        }
-    };
-    xhr.send("prénom=" + prénom +"nom=" + nom + "&email=" + email + "&password=" + password);
-}
